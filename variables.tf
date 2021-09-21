7e0f061d4195d02242df2abb9bc89f3b3b456006
ccalongesia <91119147+ccalongesia@users.noreply.github.com>
2021-09-21 12:54:12 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "subscription_id" {
   type        = string
   description = "(Required) The Subscription ID"
 }
+
+variable "location" {
+  type        = string
+  description = "(Required) Specifies the supported Azure location where the resource exists. Changing this forces a new resource to be created."
+}
